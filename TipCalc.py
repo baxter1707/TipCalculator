8e69d255adc1c93086f3e3c7dc1a55b48c223199
Michael Baxter <msb@Michaels-MBP.localdomain>
2017-11-14 13:04:33 -0600

    Tip Calculator

---
+++ TipCalc.py
@@ -0,0 +1,8 @@
+mealTotal = raw_input("How much was your meal?")
+tipPercent = raw_input("How much would you like to tip?")
+
+i = float(mealTotal)
+x= float(tipPercent)
+sum = float(i) * float(x)
+
+print(sum)
